ace1dbc2dbbc4a7d54e794a5b6510fdcef5a09f2
khalid21456 <khalidedaoudi8@gmail.com>
2026-01-25 20:42:54 +0100

    admin dashboard setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,31 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-
+import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from '@clerk/clerk-react';
+import "./index.css"
+import { Routes, Route, Navigate} from 'react-router';
+import Login from './pages/LoginPage.jsx';
+import DashboardPage from "./pages/DashboardPage.jsx"
+import ProductsPage from "./pages/ProductPage.jsx"
+import OrdersPage from "./pages/OrdersPage.jsx"
+import CustomersPage from "./pages/CustomersPage.jsx"
+import DashboardLayout from './layouts/DashboardLayout.jsx';
+import PageLoader from './components/PageLoader.jsx';
 function App() {
+
+  const {isSignedIn, isLoaded} = useAuth()
+
+  if(!isLoaded) return <PageLoader/>
+  
+  
   return (
-    <header>
-      <SignedOut>
-        <SignInButton mode="modal"/>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <Routes>
+     <Route path="/login" element={isSignedIn ? <Navigate to={"/dashboard"}/> : <Login/>}/> 
+     <Route path="/" element={isSignedIn ? <DashboardLayout/> : <Login/>}>
+      <Route index element={<Navigate to={"/dashboard"}/>}/>
+      <Route path="dashboard" element={<DashboardPage/>}/>
+      <Route path="products" element={<ProductsPage/>}/>
+      <Route path="orders" element={<OrdersPage/>}/>
+      <Route path="customers" element={<CustomersPage/>}/>
+     </Route>
+    </Routes>
   )
 }
 
