a7b8ceb2c40c715c8d56eb0ac4b68a64dacf843e
khalid21456 <khalidedaoudi8@gmail.com>
2026-01-29 20:06:01 +0100

    expo setup configuration

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 export default function Login() {
     return (
-       <div>
+       <div className="h-screen hero">
         <SignIn></SignIn>
        </div> 
     );
