a07ee5a646b6d8f07134363ad1c35cf4d473ba55
khalid21456 <khalidedaoudi8@gmail.com>
2026-01-30 21:32:05 +0100

    implement oauth in mobile folder

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,10 +1,70 @@
-import { View, Text } from "react-native";
-
+import useSocialAuth from "@/hooks/useSocialHook";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 
 export default function AuthScreen() {
-    return(
-        <View>
-            <Text>AuthScreen</Text>
-        </View>
-    )
+  const { isLoading, handleSocialAuth } = useSocialAuth();
+
+  return (
+    <View className="flex-1 px-8r justify-center items-center bg-white">
+      <Image
+        source={require("../../assets/images/auth-image.png")}
+        className="size-96"
+        resizeMode="contain"
+      />
+      <View className="gap-2 mt-4">
+        <TouchableOpacity
+          onPress={() => handleSocialAuth("oauth_google")}
+          disabled={isLoading}
+          className="flex-row items-center justify-center bg-white border px-12 py-3 border-gray-300 rounded-full px-6 shadow shadow-slate-400"
+        >
+          {isLoading ? (
+            <ActivityIndicator size={"small"} color={"#4285f4"} />
+          ) : (
+            <View className="flex-row items-center justify-center">
+              <Image
+                source={require("../../assets/images/google.png")}
+                className="size-10 mr-3"
+                resizeMode="contain"
+              />
+              <Text className="text-black text-base font-medium">
+                Continue With Google
+              </Text>
+            </View>
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => handleSocialAuth("oauth_apple")}
+          disabled={isLoading}
+          className="flex-row items-center justify-center bg-white border px-12 py-4 border-gray-300 rounded-full px-6 shadow shadow-slate-400"
+        >
+          {isLoading ? (
+            <ActivityIndicator size={"small"} color={"#4285f4"} />
+          ) : (
+            <View className="flex-row items-center justify-center">
+              <Image
+                source={require("../../assets/images/apple.png")}
+                className="size-8 mr-3"
+                resizeMode="contain"
+              />
+              <Text className="text-black text-base font-medium">
+                Continue With Apple
+              </Text>
+            </View>
+          )}
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center text-gray-500 text-xs leading-4 mt-6 px-2">
+          By Signing up, you agree to our <Text className="text-blue-500 ">Terms</Text>, 
+          <Text className="text-blue-500"> Privacy Policy </Text> 
+          and 
+          <Text className="text-blue-500"> Cookie Use</Text>.
+      </Text>
+    </View>
+  );
 }
